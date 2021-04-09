caf88315729e596034d17d28f0cddb5bd061a087
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-04-09 23:43:57 +0600

    Initializes simbox css framework with basic scss setup

---
+++ README.md
@@ -0,0 +1,31 @@
+## SimBox
+A CSS framework with Simplicity and Box layout which focuses on performance and simplicity.
+
+----
+
+
+#### Goal
+Welcome to the world of SimBox CSS. It's a CSS framework which primary goal is to make a simple css framework with less effort and headache.
+
+#### Version
+0.0.1
+
+#### Note
+
+| :exclamation:  Currently SimBox is under development mode, so don't use it in your production mode. It'll be published it's fist version soon.   |
+|-----------------------------------------|
+
+
+#### Todos:
+- [x] Create `Scss` or `Sass` Based CSS architecture
+- [ ] Create environment variables for all
+- [ ] CSS For Buttons
+- [ ] CSS For Card
+- [ ] CSS For Alert
+- [ ] CSS For Table
+
+#### License
+MIT
+
+#### Contribution
+Yes, Just create a pull request and you can contribute very easily.
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const TerserJSPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+    entry: './src',
+    mode: 'development',
+
+    // Output: output directory links
+    output: {
+        path: __dirname + "/dist",
+        filename: 'js/simbox.min.js'
+    },
+
+    // Optimization: minify the js and css files plugins
+    optimization: {
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                exclude: /(node_modules|bower_components)/,
+                include: __dirname + '/src',
+            },
+
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                ]
+            },
+
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            }
+        ],
+    },
+
+    plugins: [
+
+        // Plugins that will make a css file after converting from scss and extract to location
+        new MiniCssExtractPlugin({
+            filename: 'css/simbox.min.css',
+            chunkFilename: '[id].min.css',
+            ignoreOrder: false
+        }),
+
+    ],
+    watch: true,
+};
